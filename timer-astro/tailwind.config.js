f1dfc97b8726bafc6ac19837f47dbcbc6bc6d55e
znaim <zair.naim@thoughtworks.com>
2024-11-27 15:29:43 -0500

    added daisy and tailwind dependencies

---
+++ timer-astro/tailwind.config.js
@@ -0,0 +1,35 @@
+@ -0,0 +1,33 @@
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: [
+        {
+            custom_theme: {
+                "primary": "#CC850A",
+                "secondary": "#47A1AD",
+                "accent": "#F2617A",
+                "neutral": "#634F7D",
+                "base-100": "#01394D",
+                "info": "#CC850A",
+                "base-content": "#EDF1F3",
+            },
+        },
+        "light",
+        "dark",
+        "retro",
+    ],
+},
+}
+
+
